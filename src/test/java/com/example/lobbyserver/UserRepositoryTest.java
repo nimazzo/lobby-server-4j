7c43a97742987a6fc7c60642ad12cd2df0aa427f
nimazzo <46820077+nimazzo@users.noreply.github.com>
2024-09-10 17:16:29 +0200

    Added Test for UserRepository
    - make sure testcontainers work properly

---
+++ src/test/java/com/example/lobbyserver/UserRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.example.lobbyserver;
+
+import com.example.lobbyserver.user.User;
+import com.example.lobbyserver.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Import(TestcontainersConfiguration.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private final PasswordEncoder encoder;
+
+    public UserRepositoryTest() {
+        this.encoder = new BCryptPasswordEncoder();
+    }
+
+    @BeforeEach
+    void setUp() {
+        userRepository.save(new User(null, "admin", encoder.encode("admin"), true));
+        userRepository.save(new User(null, "user", encoder.encode("user"), true));
+    }
+
+    @Test
+    void testThatContextLoads() {
+    }
+
+    @Test
+    void testThatDatabaseWorks() {
+        var admin = userRepository.findUserByUsername("admin");
+        assertThat(admin).isNotNull();
+        assertThat(admin.getUsername()).isEqualTo("admin");
+        assertThat(encoder.matches("admin", admin.getPassword())).isTrue();
+
+        var user = userRepository.findUserByUsername("user");
+        assertThat(user).isNotNull();
+        assertThat(user.getUsername()).isEqualTo("user");
+        assertThat(encoder.matches("user", user.getPassword())).isTrue();
+
+        var numberOfRows = userRepository.count();
+        assertThat(numberOfRows).isEqualTo(2);
+    }
+}
